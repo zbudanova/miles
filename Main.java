801c5026e124b84feca4153a5943c4b493c28a09
Zarina Budanova <zarina.nurbaeva@gmail.com>
2021-09-10 16:16:41 +0500

    Fist commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket;
+        ticket = 730;
+        int miles;
+        miles= 20;
+        int bonus = ticket / miles;
+        System.out.println(bonus);
+    }
+}
